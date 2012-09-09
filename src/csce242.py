2c7138f9a13cbfb0cad6ec8bf947309b9a957770
Guilherme Salazar <gmesalazar@gmail.com>
2012-09-08 21:39:21 -0400

    HW2 submission

--- src/csce242.py
+++ src/csce242.py
@@ -1,7 +1,8 @@
 '''
 Created on Aug 28, 2012
 
-@author: guilherme
+@author: Guilherme Salazar 
+@email gmesalazar@{acm, gmail}.com
 '''
 
 import webapp2
@@ -9,13 +10,30 @@ import jinja2
 import os
 
 jinja_environment = jinja2.Environment(
-                        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+                        loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'))
 
+'''
+@summary: Handler associated with the main page
+'''
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
-        template = jinja_environment.get_template('index.html')
+        template = jinja_environment.get_template('base.html')
         self.response.out.write(template.render())
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+'''
+@summary: Handler associated with the "display" page
+'''
+class Display(webapp2.RequestHandler):
+    def post(self):
+        
+        template_values = {
+            'url': self.request.get('url'),
+            'caption': self.request.get('caption')
+        }
+        
+        template = jinja_environment.get_template('display.html')
+        self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/display', Display)],
                               debug=True)
