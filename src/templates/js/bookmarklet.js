da928a9375608da9fa45fe7a2f5b74cf15a86d50
Guilherme Salazar <gmesalazar@gmail.com>
2012-10-08 02:59:12 -0400

    HW6 Submission

---
+++ src/templates/js/bookmarklet.js
@@ -0,0 +1,67 @@
+console.log('loaded bookmarklet.js')
+
+imgsRef = document.getElementsByTagName('img');
+
+imgsArr = new Array()
+
+for (i = 0; i < imgsRef.length; i++) {
+	imgElem = document.createElement('img');
+	imgElem.src = imgsRef[i].src;
+	
+	imgsArr.push(imgElem);
+}
+
+document.body.innerHTML = '';
+
+h1 = document.createElement('h1');
+h1.innerText = 'Pinboard';
+
+document.body.appendChild(h1);
+
+hostName = 'csce242-gmesalazar.appspot.com'
+locationHost = 'http://' + hostName + '/pin/';
+
+for (i = 0; i < imgsArr.length; i++) {
+	
+	div = document.createElement('div');
+	div.style.display = 'table';
+	div.style.position = 'relative';
+	div.style.margin = 'auto';
+	div.style.height = 'auto';
+	div.style.width = 'auto';
+	div.style.borderRadius = '8px';
+	div.style.boxShadow = '0px 0px 6px #73000A';
+	
+	form = document.createElement('form');
+	form.setAttribute('method', 'post');
+	form.setAttribute('action', locationHost);
+	
+	label = document.createElement('label');
+	label.innerText = 'Caption: ';
+	
+	urlBox = document.createElement('input');
+	urlBox.setAttribute('type', 'hidden');
+	urlBox.setAttribute('name', 'url');
+	urlBox.setAttribute('value', imgsArr[i].src);
+	
+	captionBox = document.createElement('input');
+	captionBox.setAttribute('type', 'text');
+	captionBox.setAttribute('name', 'caption');
+	
+	button = document.createElement('input');
+	button.setAttribute('type', 'submit');
+	button.setAttribute('name', 'submitButton');
+	button.setAttribute('value', 'This one');
+	
+	form.appendChild(label);
+	form.appendChild(urlBox);
+	form.appendChild(captionBox);
+	form.appendChild(button);
+	
+	div.appendChild(imgsArr[i]);
+	div.appendChild(document.createElement('br'));
+	div.appendChild(form);
+	
+	document.body.appendChild(div);
+	document.body.appendChild(document.createElement('br'));
+}
