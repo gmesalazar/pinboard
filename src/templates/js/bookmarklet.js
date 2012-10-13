481995645ff03391babc206d12f293998b4eb167
Guilherme Salazar <gmesalazar@gmail.com>
2012-10-13 16:43:26 -0400

    A few visual adjustments

--- src/templates/js/bookmarklet.js
+++ src/templates/js/bookmarklet.js
@@ -13,12 +13,18 @@ for (i = 0; i < imgsRef.length; i++) {
 
 document.body.innerHTML = '';
 
+document.body.style.margin = '8px';
+document.body.style.background = '#D0D4D2';
+
+document.body.appendChild(document.createElement('br'));
+
 h1 = document.createElement('h1');
 h1.innerText = 'Pinboard';
 
 document.body.appendChild(h1);
 
-hostName = 'csce242-gmesalazar.appspot.com'
+//hostName = 'csce242-gmesalazar.appspot.com';
+hostName = 'localhost:8080';
 locationHost = 'http://' + hostName + '/pin/';
 
 for (i = 0; i < imgsArr.length; i++) {
@@ -29,8 +35,13 @@ for (i = 0; i < imgsArr.length; i++) {
 	div.style.margin = 'auto';
 	div.style.height = 'auto';
 	div.style.width = 'auto';
-	div.style.borderRadius = '8px';
+	div.style.borderRadius = '4px';
 	div.style.boxShadow = '0px 0px 6px #73000A';
+	div.style.padding = '8px';
+	
+	imgsArr[i].style.display = 'block';
+	imgsArr[i].style.marginLeft = 'auto';
+	imgsArr[i].style.marginRight = 'auto';
 	
 	form = document.createElement('form');
 	form.setAttribute('method', 'post');
@@ -38,6 +49,8 @@ for (i = 0; i < imgsArr.length; i++) {
 	
 	label = document.createElement('label');
 	label.innerText = 'Caption: ';
+	label.style.float = 'left';
+	label.style.width = '100px';
 	
 	urlBox = document.createElement('input');
 	urlBox.setAttribute('type', 'hidden');
@@ -47,15 +60,21 @@ for (i = 0; i < imgsArr.length; i++) {
 	captionBox = document.createElement('input');
 	captionBox.setAttribute('type', 'text');
 	captionBox.setAttribute('name', 'caption');
+	captionBox.style.display = 'inline';
+	captionBox.style.float = 'right';
 	
 	button = document.createElement('input');
 	button.setAttribute('type', 'submit');
 	button.setAttribute('name', 'submitButton');
 	button.setAttribute('value', 'This one');
 	
+	button.style.float = 'right';
+	
 	form.appendChild(label);
 	form.appendChild(urlBox);
 	form.appendChild(captionBox);
+	form.appendChild(document.createElement('br'));
+	form.appendChild(document.createElement('br'));
 	form.appendChild(button);
 	
 	div.appendChild(imgsArr[i]);
