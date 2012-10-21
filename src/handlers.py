gmesalazar/pinboard

commit fee560bddaaf8d4b7152ecbf432c6376cddcb91b

Refactor the code and organize the repo

src/handlers.py
@@ -1,0 +1,386 @@
+'''
+Created on Oct 18, 2012
+
+@author: Guilherme Salazar 
+@email gmesalazar@gmail.com
+'''
+
+import webapp2
+import os
+import jinja2
+
+from datamodels import *
+from google.appengine.api import users
+
+jinja_environment = jinja2.Environment(
+                        loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'))
+
+'''
+@summary: Class containing misc methods
+'''
+class Util(webapp2.RedirectHandler):
+
+    def render(self, tempName):
+        template = jinja_environment.get_template(tempName)
+        self.response.out.write(template.render(self.template_values))
+        
+    def exceptionRender(self, message, tempName):
+        
+        user = users.get_current_user()
+        
+        if user:
+            
+            self.template_values = {
+                    'username': user.nickname() + ' ',
+                    'headurl': users.create_logout_url("/"),
+                    'text': 'Logout',
+                    'exception': True,
+                    'message_header': 'Exceptional condition detected!',
+                    'message_body':  message
+                }           
+            
+            self.render(tempName)
+        
+        else:
+            self.redirect('/login')
+            
+    def getObjectFromDatastore(self, path, objId):
+        key = db.Key.from_path(path, objId)
+        return db.get(key)
+
+class MainPage(Util):
+    
+    def get(self):
+        
+        user = users.get_current_user()
+        
+        if user:
+            anonymous = False
+            username = user.nickname()
+            headUrl = users.create_logout_url('/')
+            text = 'Logout'            
+        else:
+            anonymous = True
+            username = ''
+            headUrl = users.create_login_url('/')
+            text = 'Login'    
+           
+        self.template_values = {
+            'anonymous': anonymous,
+            'username': username,
+            'headurl': headUrl,
+            'text': text
+        }         
+        
+        self.render('home.html')
+            
+class PinHandler(Util):
+    
+    def get(self, pinid):
+
+        pinid = long(pinid)
+
+        user = users.get_current_user()        
+        
+        if not user:
+            self.redirect('/login')
+            return
+
+        pin = self.getObjectFromDatastore('Pin', pinid)
+        
+        if not pin:
+            self.exceptionRender('Pin not found; you were redirected to the main page.',
+                                 'home.html')
+            return
+
+        if not pin.private or pin.owner == user:
+        
+            pins = set()
+            pins.add(pin)
+            
+            boards = set()
+            
+            for boardid in pin.boards:
+                board = self.getObjectFromDatastore('Board', boardid)
+                boards.add(board)
+            
+            self.template_values = {
+                'items': pins,
+                'boards': boards,
+                'username': user.nickname(),
+                'headurl': users.create_logout_url('/'),
+                'text': 'Logout',
+                'objectname': pin.caption,
+                'pagecat': 'Pin',
+                'anonymous': not user,
+                'editable': user == pin.owner
+            }
+            
+            self.render('pin.html')
+        
+    def post(self, pinid):
+        
+        pinid = long(pinid)
+        
+        user = users.get_current_user()
+        
+        key = db.Key.from_path('Pin', pinid)
+        pin = db.get(key)
+        
+        if user == pin.owner:
+        
+            self.action = self.request.get('action')
+        
+            if self.action == 'update':
+                newCaption = self.request.get('caption')
+                
+                pin.caption = newCaption
+                
+                checkbox = self.request.get('privacy')
+                
+                if checkbox == 'true':
+                    pin.private = True
+                else:
+                    pin.private = False
+                
+                pin.save()
+                
+            elif self.action == 'delete':
+                key = db.Key.from_path('Pin', pinid)
+                pin = db.get(key)
+                
+                for boardid in pin.boards:
+                    board = self.getObjectFromDatastore('Board', boardid)
+                    board.pins.remove(pinid)
+                    board.save()
+                
+                pin.delete()
+                self.redirect('/pin')
+            
+class PinsHandler(Util):
+    
+    def get(self):
+        
+        user = users.get_current_user()
+        
+        if not user:
+            self.redirect('login')
+            return
+        
+        # Quick and dirty solution for the absence of an OR operator... :p
+        self.q1 = Pin.all().filter("private =", False).fetch(1000)
+        self.q2 = Pin.all().filter("owner", user).filter("private =", True).fetch(1000)
+        self.pins = [] + self.q1 + self.q2
+        
+        self.template_values = {
+            'items': self.pins,
+            'username': user.nickname() + ' ',
+            'headurl': users.create_logout_url('/'),
+            'text': 'Logout',
+            'pagecat': 'Pins',
+            'editable': True
+        }
+    
+        self.render('pthumbs.html')
+        
+    def post(self):
+        
+        pin = Pin(imgUrl=self.request.get('url'), caption=self.request.get('caption'), boards=[])
+        
+        checkbox = self.request.get('privacy')
+        
+        if checkbox == 'private':
+            pin.private = True
+        else:
+            pin.private = False
+        
+        pin.save()
+        pin.id = pin.key().id()
+        pin.save()
+        
+        self.redirect('/pin/%s' % pin.key().id())
+
+class BoardHandler(Util):
+    
+    def get(self, pinid):
+        
+        boardid = long(pinid)
+        user = users.get_current_user()
+        
+        if user:
+            username = user.nickname()
+            headUrl = users.create_logout_url('/')
+            headText = 'Logout'
+        
+        else:
+            username = 'anonymous'
+            headUrl = users.create_login_url('/')
+            headText = 'Login'
+        
+        board = self.getObjectFromDatastore('Board', boardid)
+        
+        if not board:
+            self.exceptionRender('Board not found; you were redirected to the main page.',
+                                 'home.html')
+            return
+            
+        if not board.private or board.owner == user:
+            
+            allpins = Pin.all().filter("owner =", user)
+            
+            boardpins = set()
+            for pinid in board.pins:
+                pin = self.getObjectFromDatastore('Pin', pinid)
+                boardpins.add(pin)
+            
+            self.template_values = {
+                'board': board,
+                'allpins': allpins,
+                'items': boardpins,
+                'username': username,
+                'headurl': headUrl,
+                'text': headText,
+                'objectname': board.title,
+                'pagecat': 'Board',
+                'anonymous': not user,
+                'editable': user == board.owner
+            }
+        
+            self.render('board.html')                  
+
+    def post(self, boardid):
+        
+        boardid = long(boardid)
+        board = self.getObjectFromDatastore('Board', boardid)
+        
+        user = users.get_current_user()
+        
+        if user == board.owner:
+        
+            action = self.request.get('action')
+            
+            if action == 'update':
+                newTitle = self.request.get('title')
+                newUrl = self.request.get('coverurl')
+                
+                board.title = newTitle
+                board.imgUrl = newUrl
+                
+                checkbox = self.request.get('privacy')
+                
+                if checkbox == 'private':
+                    board.private = True
+                else:
+                    board.private = False      
+    
+                if self.request.get("addpinlist") != '--':
+                    pinid = long(self.request.get("addpinlist"))
+                    board.pins.append(pinid)
+                    pin = self.getObjectFromDatastore('Pin', pinid)
+                    pin.boards.append(board.key().id())
+                    pin.save()
+                
+                if self.request.get("delpinlist") != '--':
+                    pinid = long(self.request.get("delpinlist"))
+                    board.pins.remove(pinid)
+                    pin = self.getObjectFromDatastore('Pin', pinid)
+                    pin.boards.remove(board.key().id())
+                    pin.save()
+                
+                board.save()
+                self.redirect('/board/%s' % boardid)
+                
+            elif action == 'delete':
+                board = self.getObjectFromDatastore('Board', boardid)
+                
+                for pinid in board.pins:
+                    pin = self.getObjectFromDatastore('Pin', pinid)
+                    pin.boards.remove(board.key().id())
+                    pin.save()
+                
+                board.delete()
+                self.redirect('/board')
+
+class BoardsHandler(Util):
+    
+    def get(self):
+        user = users.get_current_user()
+        
+        if user:
+            anonymous = False
+            username = user.nickname()
+            headUrl = users.create_logout_url('/')
+            headText = 'Logout'
+        else:
+            anonymous = True
+            username = ''
+            headUrl = users.create_login_url('/')
+            headText = 'Login'
+        
+        # Quick and dirty solution for the absence of an OR operator... :p
+        q1 = Board.all().filter("private =", False).fetch(1000)
+        q2 = Board.all().filter("owner =", user).filter("private =", True).fetch(1000)
+        boards = [] + q1 + q2
+        
+        pins = Pin.all().filter("owner =", user)
+            
+        self.template_values = {
+            'allpins': pins,
+            'items': boards,
+            'username': username,
+            'headurl': headUrl,
+            'text': headText,
+            'anonymous': anonymous,
+            'pagecat': 'Boards' 
+        }
+    
+        self.render('bthumbs.html')
+            
+
+    def post(self):
+
+        board = Board(title=self.request.get('title'), imgUrl=self.request.get('coverurl'), pins=[])
+        
+        checkbox = self.request.get('privacy')
+        
+        if checkbox == 'private':
+            board.private = True
+        else:
+            board.private = False
+        
+        if self.request.get("addpinlist") != '--':
+            pinid = long(self.request.get("addpinlist"))
+            pin = self.getObjectFromDatastore('Pin', pinid)
+            board.save()
+            pin.boards.append(long(board.key().id()))
+            pin.save()
+        
+        board.save()
+        board.id = board.key().id()
+        board.save()
+        
+        self.redirect('/board/%s' % board.key().id())
+   
+'''
+@summary: Class containing authentication handlers
+'''
+class LoginHandler(Util):
+    
+    def get(self):
+        
+        self.template_values = {
+            'username': '',
+            'headurl': users.create_login_url('/'),
+            'text': 'Login'
+        }
+        
+        self.render('login.html')
+
+
+'''
+@summary: 404 error handler
+'''
+class NotFoundPageHandler(Util):
+    
+    def get(self):
+        self.exceptionRender('Page not found; you were redirected to the main page.', 'home.html')

src/templates/board.html
@@ -35,15 +35,15 @@
 		<select id="delpinlist" name="delpinlist"> 
 		    <option value="--">--</option>
 		    
-		  {% for pin in items %}
-			<option value="{{pin.id}}">{{ pin.caption }}</option>
+		  {% for pin in boardpins %}
+	      <option value="{{pin.id}}">{{ pin.caption }}</option>
 		  {% endfor%}
 		  
 		</select> 
 		
 		<br /> <br />
 		
-		<input type="checkbox" name="privacy" value="private"{{private}}> Private 
+		<input type="checkbox" name="privacy" value="private" {% if board.private %} checked="checked" {% endif %} /> Private 
 		
 		<br /> <br />
 		

src/templates/bthumbs.html
@@ -1,26 +1,13 @@
 {% extends "base.html" %}
 
-{% block header %}
-<div id="header">
-
-	<p class="hfp">
-		<span class="lefttxt"><a href="/">Home</a></span> 
-		
-		<span class="righttxt">
-		  {% if editable %}
-			<a href="/board">My Boards</a> - <a href="/pin">My Pins</a> - <b>{{username}}</b> -
-		  {% endif %}
-			<a href="{{headurl}}">{{text}}</a>
-		</span>
-	</p>
-
-</div>
-{% endblock %}
-
 <!-- Block that appears before "Enter new pin" -->
 {% block display %}
+{% if anonymous and items|count == 0 %}
+<div class="box">
+    <p>There are no public boards!</p>
+</div>
 
-{% if items|count > 0 or editable %}
+{% elif items|count > 0 %}
 
 {% for item in items %}
 <div class="box">
@@ -28,44 +15,45 @@
 		<a href={{"/board/" ~ item.id }}>
 		  
 		  {% if item.imgUrl %}
-		      <img class="thumbs" width="200" align="top" src={{ '"' + item.imgUrl + '"' }}/>
+		      <img class="thumbs" width="200" align="top" src="{{item.imgUrl}}"/>
 		  {% else %}
-		      <img class="thumbs" align="top" src="/templates/images/board.png"/>
+		      <img class="thumbs" align="top" src="/static/images/board.png"/>
 		  {% endif %}
 		  
 		</a>
-	</p>
-	<p class="centralized">
-		{{ item.title }}<br /> Owner: {{item.owner }}
+		
+		<div class="centralized">
+            {{ item.title }}<br /> Owner: {{item.owner }}
+        </div>
 	</p>
 </div>
 <br />
 {% endfor %}
-
-{% else %}
-<div class="box">
-    <p>There are no public boards!</p>
-</div>
 {% endif %}
 
 {% endblock %}
 
 {% block box %}
-
-{% if editable %}
+{% if not anonymous %}
 <div class="box">
 
 	<p>New board</p>
 
 	<form action="/board/" method="post">
-		<label for="titlebox">Title: </label> <input id="titletbox"
-			type="text" name="title" /><br /> <br /> <label for="coverbox">Cover
-			URL: </label> <input id="coverbox" type="text" name="coverurl" /><br /> <br />
+		<label for="titlebox">Title: </label> 
+		<input id="titletbox" type="text" name="title" />
+		
+		<br /> <br /> 
+		
+		<label for="coverbox">Cover URL: </label> 
+		<input id="coverbox" type="text" name="coverurl" />
+		
+		<br /> <br />
 
 		<label for="addpinlist">Add pin: </label> 
 		<select id="addpinlist" name="addpinlist">
+		
 		   <option value="--">--</option>
-		   
 		  {% for pin in allpins %}
 		      <option value="{{pin.id}}">{{ pin.caption }}</option>
 		  {% endfor %}

src/templates/pin.html
@@ -22,20 +22,20 @@ Part of:
 {% block pinfo %}
 
 {% if editable %}
-    <script type="text/javascript" src="/templates/js/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="/templates/js/script.js"></script>
+    <script type="text/javascript" src="/static/js/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="/static/js/script.js"></script>
 
 	<form>
-        <input id="privacy" type="checkbox" name="privacy" value="private" {{ private }} /> Private
+	   <input id="privacy" type="checkbox" name="privacy" value="private" {% if (items|first).private %} checked="checked" {% endif %} /> Private
 	</form>
 	
 	<br/>
 	
 	<form action="" method="post">
-		<span class="rightalign">
-		  <input type="hidden" name="action" value="delete" /> 
-		  <input type="submit" value="Delete this pin" />
-		</span>
+	   <span class="rightalign">
+	       <input type="hidden" name="action" value="delete" />
+	       <input type="submit" value="Delete this pin" />
+	   </span>
 	</form>
 {% endif %}
 

src/app.yaml
@@ -6,17 +6,11 @@ threadsafe: true
 
 handlers:
 
-- url: /templates/stylesheets
-  static_dir: templates/stylesheets
+- url: /static/
+  static_dir: static/
   
-- url: /templates/images
-  static_dir: templates/images
-
-- url: /templates/js
-  static_dir: templates/js
-
 - url: /.*
-  script: csce242.app
+  script: main.app
   
 libraries:
 - name: jinja2

commit 24c0b2b4effc6b699f00d2809f3f56833b1534d7

Fix bugs introduced with commit fee560b

src/handlers.py
@@ -291,7 +291,6 @@ class BoardHandler(Util):
                 self.redirect('/board/%s' % boardid)
                 
             elif action == 'delete':
-                board = self.getObjectFromDatastore('Board', boardid)
                 
                 for pinid in board.pins:
                     pin = self.getObjectFromDatastore('Pin', pinid)
@@ -350,6 +349,7 @@ class BoardsHandler(Util):
         
         if self.request.get("addpinlist") != '--':
             pinid = long(self.request.get("addpinlist"))
+            board.pins.append(pinid)
             pin = self.getObjectFromDatastore('Pin', pinid)
             board.save()
             pin.boards.append(long(board.key().id()))

src/templates/board.html
@@ -35,7 +35,7 @@
 		<select id="delpinlist" name="delpinlist"> 
 		    <option value="--">--</option>
 		    
-		  {% for pin in boardpins %}
+		  {% for pin in items %}
 	      <option value="{{pin.id}}">{{ pin.caption }}</option>
 		  {% endfor%}
 		  

src/templates/bthumbs.html
@@ -62,17 +62,6 @@
 		
 		<br /> <br /> 
 		
-		<label for="delpinlist">Delete pin: </label> 
-		<select id="delpinlist" name="delpinlist"> 
-			<option value="--">--</option>
-			
-			{% for pin in boardpins %}
-			<option value="{{pin.id}}">{{ pin.caption }}</option> 
-			{% endfor %}
-		</select> 
-		
-		<br /> <br /> 
-		
 		<input type="checkbox" name="privacy" value="private" checked="checked">Private 
 		
 		<br /> <br /> 

src/templates/pin.html
@@ -8,10 +8,10 @@ Part of:
 <br />
 {% for board in boards %}
 <a href="/board/{{board.id}}">{{board.title}}</a> board
-<br /> <br />
+<br />
 {% endfor %}
 </div>
-
+<br />
 {% endif %}
 {% endblock %}
 